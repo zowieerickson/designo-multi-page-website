b4846fc5cf8c074831e2dcb3eda5d9f63aa9495c
Zowie Erickson <zowie.erickson@gmail.com>
2023-08-07 08:34:53 -0400

    Bring in footer copy

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,12 +5,22 @@ import "../styles/footer.css"
 export default function Footer() {
     return (
         <footer className="globalfooter" id="globalfooter" lang="en-US">
-            <h1>this is a footer</h1>
-            <h1>this is a footer</h1>
-            <h1>this is a footer</h1>
-            <h1>this is a footer</h1>
-            <h1>this is a footer</h1>
             <Nav logoImage={LightLogo} />
+            <section className="contact">
+                <div className="contact-address">
+                    <h4>Designo Central Office</h4>
+                    <p>3886 Wellington Street</p>
+                    <p>Toronto, Ontario M9C 3J5</p>
+                </div>
+                <div className="contact-methods">
+                    <h4>Contact Us (Central Office)</h4>
+                    <p>P : +1 253-863-8967</p>
+                    <p>M : contact@designo.co</p>
+                </div>
+                <div className="contact-social">
+
+                </div>
+            </section>
         </footer>
     )
 }
